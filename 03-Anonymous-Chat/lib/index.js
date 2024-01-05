981b64cd86462ea35ed18240b34643b9c4e543e1
juan bernal <mesieou@gmail.com>
2024-01-06 09:49:06 +1100

    implementing the post messages feature

--- 03-Anonymous-Chat/lib/index.js
+++ 03-Anonymous-Chat/lib/index.js
@@ -1,18 +1,42 @@
 const batch = 1; // change to your own batch id
 const baseUrl = "https://wagon-chat.herokuapp.com/general/messages";
+const refreshButton = document.querySelector('#refresh')
+const yourMessage = document.querySelector('#your-message').value
+const yourName = document.querySelector('#your-Name').value
+const form = document.querySelector('#comment-form')
+let list = document.querySelector(".list-unstyled")
 
-// Your turn to code!
-const list = document.querySelector(".list-unstyled")
-let currentTime = new Date
+const currentTime = new Date
+let message = { name: yourName, body: yourMessage }
 
-fetch(baseUrl)
-  .then(response => response.json())
-  .then(data => {
-    const messagesHtml = data.messages.map(message => {
-      let createdTime = new Date(message.created_at)
-      let minutesDifference = Math.floor((currentTime - createdTime) / (1000 * 60))
-      `<li>${message.content} (posted <span class="date">${minutesDifference} minutes ago</span>) by ${message.author}</li>`
-    }).join('');
+const options = {
+  method: 'POST',
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(message)
+}
 
-    list.innerHTML(messagesHtml)
-  });
+const fetchMessages = () => {
+  fetch(baseUrl)
+    .then(response => response.json())
+    .then(data => {
+      let messagesHtml = data.messages.map(message => {
+        let createdTime = new Date(message.created_at)
+        let minutesDifference = Math.floor((currentTime - createdTime) / (1000 * 60))
+        `<li>${message.content} (posted <span class="date">${minutesDifference} minutes ago</span>) by ${message.author}</li>`
+      }).join('');
+      console.log(messagesHtml)
+      list.innerHTML = messagesHtml
+    });
+}
+
+const postMessage = () => {
+  fetch(baseUrl, message)
+    .then(console.log(response))
+
+}
+
+
+
+
+refreshButton.addEventListener('click', event => fetchMessages());
+form.addEventListener('submit', event => postMessage());
