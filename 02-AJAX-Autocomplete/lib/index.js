715b5aa1472479fd40f93eba929ef54b09c3eebf
juan bernal <mesieou@gmail.com>
2024-01-05 16:52:10 +1100

    added the ajax word api

--- 02-AJAX-Autocomplete/lib/index.js
+++ 02-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,25 @@
 // TODO: Autocomplete the input with AJAX calls.
+const input = document.querySelector('#search')
+let results = document.querySelector('#results')
+
+input.addEventListener('keyup', event => {
+  let word = event.currentTarget.value;
+  let apiUrl = `https://wagon-dictionary.herokuapp.com/autocomplete/${word}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+      // if (response.status === 200) {
+      //   console.log(response.json())
+      // } else {
+      //   results.innerHTML = 'no words found'
+      // }
+    .then(data => {
+      console.log(data)
+      if (data.found !== false) {
+        let wordListHtml = data.words.map(element => `<li>${element}</li>`).join('');
+        results.innerHTML = wordListHtml
+      } else {
+        results.innerHTML = 'no words found'
+      }
+    });
+})
