c6bb63559124cdc283036372e9de917f4e329356
juan bernal <mesieou@gmail.com>
2024-01-05 16:07:27 +1100

    exercise completed

--- 01-Name-Age/lib/index.js
+++ 01-Name-Age/lib/index.js
@@ -1,10 +1,13 @@
-const agifyApiUrl = "https://api.agify.io/"
 
 const displayAge = (event) => {
   event.preventDefault();
   const firstName = document.getElementById("first-name").value;
+  let yourAge = document.getElementById("your-age");
+  const agifyApiUrl = `https://api.agify.io?name=${firstName}`
   // TODO: Display your age with an AJAX call instead of the console.log()
-  console.log(firstName);
+  fetch(agifyApiUrl)
+    .then(response => response.json())
+    .then(data => yourAge.textContent = `You're ${data.age} years old.`);
 }
 
 const form = document.getElementById("fetch-age");
