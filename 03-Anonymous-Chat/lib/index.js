d3985ed472d1817605d33b15defa3a98324dcb57
juan bernal <mesieou@gmail.com>
2024-01-06 09:36:04 +1100

    refresh chat feature completed

--- 03-Anonymous-Chat/lib/index.js
+++ 03-Anonymous-Chat/lib/index.js
@@ -1,4 +1,18 @@
 const batch = 1; // change to your own batch id
-const baseUrl = "https://wagon-chat.herokuapp.com/";
+const baseUrl = "https://wagon-chat.herokuapp.com/general/messages";
 
 // Your turn to code!
+const list = document.querySelector(".list-unstyled")
+let currentTime = new Date
+
+fetch(baseUrl)
+  .then(response => response.json())
+  .then(data => {
+    const messagesHtml = data.messages.map(message => {
+      let createdTime = new Date(message.created_at)
+      let minutesDifference = Math.floor((currentTime - createdTime) / (1000 * 60))
+      `<li>${message.content} (posted <span class="date">${minutesDifference} minutes ago</span>) by ${message.author}</li>`
+    }).join('');
+
+    list.innerHTML(messagesHtml)
+  });
