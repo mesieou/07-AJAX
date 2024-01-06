d42f49f09b2a22aba7611d9877fa4559ba1c304f
juan bernal <mesieou@gmail.com>
2024-01-06 15:34:07 +1100

    added confirmation alert after creating a comment and clear the entered info

--- 03-Anonymous-Chat/lib/index.js
+++ 03-Anonymous-Chat/lib/index.js
@@ -1,55 +1,48 @@
 const batch = 1; // change to your own batch id
 const baseUrl = "https://wagon-chat.herokuapp.com/general/messages";
-const refreshButton = document.querySelector('#refresh')
-const form = document.querySelector('#comment-form')
-let list = document.querySelector(".list-unstyled")
-const currentTime = new Date
+const refreshButton = document.querySelector('#refresh');
+const form = document.querySelector('#comment-form');
+const currentTime = new Date;
+let list = document.querySelector(".list-unstyled");
+
+const formatMessageToHtml = (message) => {
+    let createdTime = new Date(message.created_at);
+    let minutesDifference = Math.max(0, Math.floor((currentTime - createdTime) / (1000 * 60)));
+      return `<li>${message.content} (posted <span class="date">${minutesDifference} minutes ago</span>) by ${message.author}</li>`
+}
+
+const insertMessageToHtml = (data) => {
+  let messagesHtml = data.messages.map((message) => formatMessageToHtml(message));
+  list.innerHTML = messagesHtml.join('');
+}
 
 const fetchMessages = (event) => {
   event.preventDefault()
   fetch(baseUrl)
     .then(response => response.json())
-    .then(data => {
-      let messagesHtml = data.messages.map((message) => {
-        let createdTime = new Date(message.created_at)
-        let minutesDifference = Math.max(0, Math.floor((currentTime - createdTime) / (1000 * 60)))
-        console.log(createdTime)
-        console.log(currentTime)
-        console.log(minutesDifference)
-
-        return `<li>${message.content} (posted <span class="date">${minutesDifference} minutes ago</span>) by ${message.author}</li>`
-      }).join('');
-      list.innerHTML = messagesHtml
-    });
+    .then(data => insertMessageToHtml(data));
 }
 
 const postMessage = (event) => {
   event.preventDefault()
-  const yourMessage = document.querySelector('#your-message').value
-  const yourName = document.querySelector('#your-name').value
-  let message = { "author": yourName , "content": yourMessage }
+  let yourMessage = document.querySelector('#your-message')
+  let yourName = document.querySelector('#your-name')
+  const message = { "author": yourName.value , "content": yourMessage.value }
 
-
-  let options = {
+  const options = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(message)
   }
 
-  fetch(baseUrl, {
-    method: 'POST',
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(message)
-  })
+  fetch(baseUrl, options)
     .then(response => response)
-    .then(data => console.log(data))
-    .catch(error => {
-      console.error('Error:', error)});
-
+    .then(data => {
+      alert('Created!')
+      yourMessage.value = ''
+      yourName.value = ''
+    })
 }
-
-
-
 
 refreshButton.addEventListener('click', event => fetchMessages(event));
 form.addEventListener('submit', event => postMessage(event));
