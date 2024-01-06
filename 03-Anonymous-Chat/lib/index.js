7a51de3d4f675ef6d88f9828a04d7f9e24f91e70
juan bernal <mesieou@gmail.com>
2024-01-06 15:02:34 +1100

    finished the post message feature

--- 03-Anonymous-Chat/lib/index.js
+++ 03-Anonymous-Chat/lib/index.js
@@ -1,42 +1,55 @@
 const batch = 1; // change to your own batch id
 const baseUrl = "https://wagon-chat.herokuapp.com/general/messages";
 const refreshButton = document.querySelector('#refresh')
-const yourMessage = document.querySelector('#your-message').value
-const yourName = document.querySelector('#your-Name').value
 const form = document.querySelector('#comment-form')
 let list = document.querySelector(".list-unstyled")
-
 const currentTime = new Date
-let message = { name: yourName, body: yourMessage }
 
-const options = {
-  method: 'POST',
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify(message)
-}
-
-const fetchMessages = () => {
+const fetchMessages = (event) => {
+  event.preventDefault()
   fetch(baseUrl)
     .then(response => response.json())
     .then(data => {
-      let messagesHtml = data.messages.map(message => {
+      let messagesHtml = data.messages.map((message) => {
         let createdTime = new Date(message.created_at)
-        let minutesDifference = Math.floor((currentTime - createdTime) / (1000 * 60))
-        `<li>${message.content} (posted <span class="date">${minutesDifference} minutes ago</span>) by ${message.author}</li>`
+        let minutesDifference = Math.max(0, Math.floor((currentTime - createdTime) / (1000 * 60)))
+        console.log(createdTime)
+        console.log(currentTime)
+        console.log(minutesDifference)
+
+        return `<li>${message.content} (posted <span class="date">${minutesDifference} minutes ago</span>) by ${message.author}</li>`
       }).join('');
-      console.log(messagesHtml)
       list.innerHTML = messagesHtml
     });
 }
 
-const postMessage = () => {
-  fetch(baseUrl, message)
-    .then(console.log(response))
+const postMessage = (event) => {
+  event.preventDefault()
+  const yourMessage = document.querySelector('#your-message').value
+  const yourName = document.querySelector('#your-name').value
+  let message = { "author": yourName , "content": yourMessage }
+
+
+  let options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(message)
+  }
+
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(message)
+  })
+    .then(response => response)
+    .then(data => console.log(data))
+    .catch(error => {
+      console.error('Error:', error)});
 
 }
 
 
 
 
-refreshButton.addEventListener('click', event => fetchMessages());
-form.addEventListener('submit', event => postMessage());
+refreshButton.addEventListener('click', event => fetchMessages(event));
+form.addEventListener('submit', event => postMessage(event));
