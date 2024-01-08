b87d09b16207ad061c1a5d81c26f10d08d87e934
juan bernal <mesieou@gmail.com>
2024-01-08 16:01:30 +1100

    refacor using mustache js

--- Optional-01-Recipe-Book/lib/index.js
+++ Optional-01-Recipe-Book/lib/index.js
@@ -1,3 +1,5 @@
+import Mustache from "mustachejs";
+
 const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=`;
 const searchButton = document.querySelector('#search-button');
 const searchInput = document.querySelector('#search-input');
@@ -15,19 +17,16 @@ const addBookmark = (event) => {
 const listenForBookmarks = () => {
   const bookmarksElement = document.querySelectorAll('.fa-bookmark')
   const bookmarks = Array.from(bookmarksElement);
+
   bookmarks.forEach( bookmark => {
     bookmark.addEventListener('click', event => addBookmark(event));
   });
 }
 
-const insertMeals = (data, container) => {
-  recipesContainer.innerHTML = ''
-  data.meals.forEach(meal => {
-    const clone = recipeTemplate.content.cloneNode(true);
-    clone.querySelector('img').src = meal.strMealThumb;
-    clone.querySelector('h6').textContent = meal.strMeal;
-    recipesContainer.appendChild(clone);
-  });
+const insertMeals = (data) => {
+  const mealData = {"meals": data.meals}
+  const output = Mustache.render(recipeTemplate.innerHTML, mealData)
+  recipesContainer.innerHTML = output;
   searchInput.value = ''
 }
 
@@ -37,7 +36,7 @@ const checkMealsExist = (data) => {
     searchInput.value = ''
     recipesContainer.innerHTML = ''
   } else {
-    insertMeals(data, recipesContainer)
+    insertMeals(data)
     listenForBookmarks()
   }
 }
@@ -49,10 +48,6 @@ const fetchAndInsert = (event) => {
   fetch(newUrl)
   .then(response => response.json())
   .then(data => checkMealsExist(data));
-}
-
-const addBookMarkToFavorites = (event) => {
-  console.log(event.cuurentTarget)
 }
 
 searchButton.addEventListener("click", event => fetchAndInsert(event))
