74678a4d8f38c86399ef74b135f80d123c6063e3
juan bernal <mesieou@gmail.com>
2024-01-08 12:50:43 +1100

    adding bookmarks

--- Optional-01-Recipe-Book/lib/index.js
+++ Optional-01-Recipe-Book/lib/index.js
@@ -1,31 +1,52 @@
 const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=`;
 const searchButton = document.querySelector('#search-button');
+const searchInput = document.querySelector('#search-input');
 const recipeTemplate = document.querySelector('#recipe-template');
 const recipesContainer = document.querySelector('#recipes-container');
 const favouritesContainer = document.querySelector('#favourites-container');
+const bookmarksElement = document.querySelectorAll('.fa-bookmark')
 
-let recipes = [];
+const bookmarks = Array.from(bookmarksElement);
 
 const insertMeals = (data, container) => {
+  recipesContainer.innerHTML = ''
   data.meals.forEach(meal => {
     const clone = recipeTemplate.content.cloneNode(true);
     clone.querySelector('img').src = meal.strMealThumb;
     clone.querySelector('h6').textContent = meal.strMeal;
     recipesContainer.appendChild(clone);
   });
+  searchInput.value = ''
 }
 
-const fetchAndInsert = () => {
-  const searchInput = document.querySelector('#search-input').value;
-  const newUrl = `${url}${searchInput}`
+const checkForBookmarksChecked = () => {
+  bookmarks.forEach( bookmark => {
+    bookmark.addEventListener('click', event => console.log(event))
+  });
+}
+
+const checkMealsExist = (data) => {
+  if (data.meals == null) {
+    alert("no results")
+    searchInput.value = ''
+    recipesContainer.innerHTML = ''
+  } else {
+    insertMeals(data, recipesContainer)
+    checkForBookmarksChecked()
+  }
+}
+
+const fetchAndInsert = (event) => {
+  event.preventDefault();
+  const newUrl = `${url}${searchInput.value}`
 
   fetch(newUrl)
   .then(response => response.json())
-  .then(data => insertMeals(data, recipesContainer));
+  .then(data => checkMealsExist(data));
 }
 
-searchButton.addEventListener("click", fetchAndInsert)
+const addBookMarkToFavorites = (event) => {
+  console.log(event.cuurentTarget)
+}
 
-// searchButton.addEventListener('click', fetchAndInsert())
-
-// Todo: create a function to fetch the recipes from the API when the search input changes
+searchButton.addEventListener("click", event => fetchAndInsert(event))
